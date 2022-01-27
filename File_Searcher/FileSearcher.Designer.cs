67ccef414d7884322228d0582d9fa98780371411
my.arafat <my.arafat@blmanagedservices.com>
2022-01-27 23:04:22 +0600

    Nothing important

--- File_Searcher/FileSearcher.Designer.cs
+++ File_Searcher/FileSearcher.Designer.cs
@@ -34,44 +34,46 @@
             this.DestinationFolderBtn = new System.Windows.Forms.Button();
             this.SourceFolderBtn = new System.Windows.Forms.Button();
             this.FileUploadTxtBox = new System.Windows.Forms.TextBox();
-            this.FileExtentionTxtBox = new System.Windows.Forms.TextBox();
             this.DestinationFolderTxtBox = new System.Windows.Forms.TextBox();
             this.SourceFolderTxtBox = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
             this.folderBrowserDialog2 = new System.Windows.Forms.FolderBrowserDialog();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.SearchBtn = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
             this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
             this.groupBox1.Controls.Add(this.TemplateDownloadBtn);
+            this.groupBox1.Controls.Add(this.SearchBtn);
             this.groupBox1.Controls.Add(this.FileUploadBtn);
             this.groupBox1.Controls.Add(this.DestinationFolderBtn);
             this.groupBox1.Controls.Add(this.SourceFolderBtn);
             this.groupBox1.Controls.Add(this.FileUploadTxtBox);
-            this.groupBox1.Controls.Add(this.FileExtentionTxtBox);
             this.groupBox1.Controls.Add(this.DestinationFolderTxtBox);
             this.groupBox1.Controls.Add(this.SourceFolderTxtBox);
             this.groupBox1.Controls.Add(this.label4);
-            this.groupBox1.Controls.Add(this.label3);
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Location = new System.Drawing.Point(12, 24);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(885, 171);
+            this.groupBox1.Size = new System.Drawing.Size(885, 150);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Search Criteria";
             // 
             // TemplateDownloadBtn
             // 
-            this.TemplateDownloadBtn.Location = new System.Drawing.Point(797, 96);
+            this.TemplateDownloadBtn.Location = new System.Drawing.Point(797, 78);
             this.TemplateDownloadBtn.Name = "TemplateDownloadBtn";
             this.TemplateDownloadBtn.Size = new System.Drawing.Size(67, 23);
             this.TemplateDownloadBtn.TabIndex = 2;
@@ -81,7 +83,7 @@
             // 
             // FileUploadBtn
             // 
-            this.FileUploadBtn.Location = new System.Drawing.Point(725, 96);
+            this.FileUploadBtn.Location = new System.Drawing.Point(725, 78);
             this.FileUploadBtn.Name = "FileUploadBtn";
             this.FileUploadBtn.Size = new System.Drawing.Size(67, 23);
             this.FileUploadBtn.TabIndex = 2;
@@ -111,19 +113,10 @@
             // 
             // FileUploadTxtBox
             // 
-            this.FileUploadTxtBox.Location = new System.Drawing.Point(117, 98);
+            this.FileUploadTxtBox.Location = new System.Drawing.Point(117, 80);
             this.FileUploadTxtBox.Name = "FileUploadTxtBox";
             this.FileUploadTxtBox.Size = new System.Drawing.Size(602, 20);
             this.FileUploadTxtBox.TabIndex = 1;
-            // 
-            // FileExtentionTxtBox
-            // 
-            this.FileExtentionTxtBox.Location = new System.Drawing.Point(117, 76);
-            this.FileExtentionTxtBox.Name = "FileExtentionTxtBox";
-            this.FileExtentionTxtBox.ReadOnly = true;
-            this.FileExtentionTxtBox.Size = new System.Drawing.Size(313, 20);
-            this.FileExtentionTxtBox.TabIndex = 1;
-            this.FileExtentionTxtBox.Text = ".Pdf";
             // 
             // DestinationFolderTxtBox
             // 
@@ -142,20 +135,11 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(10, 101);
+            this.label4.Location = new System.Drawing.Point(10, 83);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(23, 13);
             this.label4.TabIndex = 0;
             this.label4.Text = "File";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(10, 79);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(70, 13);
-            this.label3.TabIndex = 0;
-            this.label3.Text = "File Extention";
             // 
             // label2
             // 
@@ -177,25 +161,58 @@
             // 
             // openFileDialog1
             // 
-            this.openFileDialog1.Filter = "Excel 97-2003 (*.xls)|*.xls|Excel Workbook (*.xlsx)|*.xlsx|csv(*.csv)|*.csv";
+            this.openFileDialog1.Filter = "Excel 97-2003 (*.xls)|*.xls|Excel Workbook (*.xlsx)|*.xlsx";
             this.openFileDialog1.FilterIndex = 2;
             this.openFileDialog1.RestoreDirectory = true;
             // 
             // saveFileDialog1
             // 
-            this.saveFileDialog1.Filter = "Excel 97-2003 (*.xls)|*.xls|csv(*.csv)|*.csv";
+            this.saveFileDialog1.Filter = "Excel 97-2003 (*.xls)|*.xls";
             this.saveFileDialog1.FilterIndex = 2;
+            // 
+            // SearchBtn
+            // 
+            this.SearchBtn.Location = new System.Drawing.Point(13, 121);
+            this.SearchBtn.Name = "SearchBtn";
+            this.SearchBtn.Size = new System.Drawing.Size(67, 23);
+            this.SearchBtn.TabIndex = 2;
+            this.SearchBtn.Text = "Search";
+            this.SearchBtn.UseVisualStyleBackColor = true;
+            this.SearchBtn.Click += new System.EventHandler(this.SearchBtn_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.dataGridView);
+            this.groupBox2.Location = new System.Drawing.Point(12, 196);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(885, 306);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Display";
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.DisplayedCells;
+            this.dataGridView.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(13, 19);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.Size = new System.Drawing.Size(866, 281);
+            this.dataGridView.TabIndex = 1;
             // 
             // FileSearcher
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(909, 514);
+            this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Name = "FileSearcher";
             this.Text = "File Searcher";
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -204,11 +221,9 @@
 
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.TextBox FileUploadTxtBox;
-        private System.Windows.Forms.TextBox FileExtentionTxtBox;
         private System.Windows.Forms.TextBox DestinationFolderTxtBox;
         private System.Windows.Forms.TextBox SourceFolderTxtBox;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button SourceFolderBtn;
@@ -219,6 +234,9 @@
         private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog2;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
         public System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button SearchBtn;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.DataGridView dataGridView;
     }
 }
 
--- File_Searcher/FileSearcher.cs
+++ File_Searcher/FileSearcher.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
 using System.IO;
 using System.Windows.Forms;
 
@@ -10,7 +13,10 @@ namespace File_Searcher
         {
             InitializeComponent();
         }
-
+        public void ShowMessage(string message)
+        {
+            MessageBox.Show(message);
+        }
         private void SourceFolderBtn_Click(object sender, EventArgs e)
         {
             if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
@@ -63,16 +69,130 @@ namespace File_Searcher
                     }
                 }
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 ShowMessage(ex.Message);
             }
-            
+
+        }
+        private void SearchBtn_Click(object sender, EventArgs e)
+        {
+            List<FileInfo> fileInfos = new List<FileInfo>();
+            string sourcePath = @"C:\Users\Yeasin\Downloads\FileSearcher";
+            string destinationPath = @"C:\Users\Yeasin\Downloads\FileSearcher\Destination";
+            string filePath = @"C:\Users\Yeasin\Downloads\FileSearcher\test.xls";
+
+            //string sourcePath = SourceFolderTxtBox.Text;
+            //string destinationPath = DestinationFolderTxtBox.Text;
+            //string filePath = FileUploadTxtBox.Text;
+            if (File.Exists(filePath))
+            {
+                string extension = Path.GetExtension(filePath);
+                if (extension.ToLower().Equals(".xls") || extension.ToLower().Equals(".xlsx"))
+                {
+                    var dt = ReadExcel(filePath, extension);
+                    List<string> names = ToList(dt);
+                    foreach (var fileName in names)
+                    {
+                        var fileInfo = Search(sourcePath, fileName);
+                        string destinationFullPath = destinationPath + "\\" + fileInfo.FileName;
+                        if (File.Exists(destinationFullPath))
+                        {
+                            DateTime lastModifyDate = new DirectoryInfo(destinationFullPath).LastWriteTime;
+                            if (lastModifyDate > fileInfo.LastModified)
+                            {
+                                fileInfo.FilePath = destinationFullPath;
+                                fileInfo.LastModified = new DirectoryInfo(destinationFullPath).LastWriteTime;
+                            }
+                            else
+                            {
+                                File.Copy(fileInfo.FilePath, destinationFullPath, true);
+                            }
+                            
+                        }
+                        else
+                        {
+                            File.Copy(fileInfo.FilePath, destinationFullPath);
+                        }
+                        
+                        fileInfos.Add(fileInfo);
+                    }
+                }
+                dataGridView.DataSource = fileInfos;
+                ShowMessage("Successfully Copied");
+            }
+        }
+        public DataTable ReadExcel(string fileName, string fileExt)
+        {
+            string conn;
+            DataTable dtexcel = new DataTable();
+            if (string.Compare(fileExt, ".xls", StringComparison.Ordinal) == 0)
+                conn = @"provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + fileName + ";Extended Properties='Excel 8.0;HRD=Yes;IMEX=1';"; //for below excel 2007  
+            else
+                conn = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + fileName + ";Extended Properties='Excel 12.0;HDR=NO';"; //for above excel 2007  
+            using (OleDbConnection con = new OleDbConnection(conn))
+            {
+                try
+                {
+                    OleDbDataAdapter
+                        oleAdpt = new OleDbDataAdapter("select * from [Sheet1$]",
+                            con); //here we read data from sheet1  
+                    oleAdpt.Fill(dtexcel); //fill excel data into dataTable  
+                }
+                catch (Exception ex)
+                {
+                    ShowMessage(ex.Message);
+                }
+            }
+            return dtexcel;
         }
 
-        public void ShowMessage(string message)
+        public List<string> ToList(DataTable dt)
         {
-            MessageBox.Show(message);
+            var fileNames = new List<string>();
+            foreach (DataRow row in dt.Rows)
+            {
+                string name = row.ItemArray[0].ToString();
+                if (!string.IsNullOrWhiteSpace(name))
+                    fileNames.Add(name + ".pdf");
+            }
+            return fileNames;
+        }
+
+        public List<FileInfo> Debug(string basePath, string fileName)
+        {
+            List<FileInfo> fileInfos = new List<FileInfo>();
+            var files = Directory.EnumerateFiles(basePath, fileName, SearchOption.AllDirectories); 
+            foreach (string file in files)
+            {
+                fileInfos.Add(new FileInfo() { FileName = fileName, FilePath = file, LastModified = new DirectoryInfo(file).LastWriteTime });
+            }
+
+            return fileInfos;
+        }
+
+        public FileInfo Search(string basePath, string fileName)
+        {
+            var files = Directory.EnumerateFiles(basePath, fileName, SearchOption.AllDirectories);
+            DateTime lastModifiedDate = DateTime.MinValue;
+            string lastModifiedFileName = String.Empty;
+            foreach (string file in files)
+            {
+                DateTime fileDate = new DirectoryInfo(file).LastWriteTime;
+                if (lastModifiedDate < fileDate)
+                {
+                    lastModifiedDate = fileDate;
+                    lastModifiedFileName = file;
+                }
+            }
+
+            return new FileInfo() { FileName = fileName, FilePath = lastModifiedFileName, LastModified = lastModifiedDate };
+        }
+        public class FileInfo
+        {
+            public string FileName { get; set; }
+            public string FilePath { get; set; }
+            public DateTime LastModified { get; set; }
         }
     }
 }
